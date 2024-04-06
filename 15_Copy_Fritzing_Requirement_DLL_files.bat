5d5e51351f6d0099c03ac79d385e47059b6e33cf
Kadoyu <88698663+Kadoyu@users.noreply.github.com>
2024-04-06 09:37:09 +0900

    Update 15_Copy_Fritzing_Requirement_DLL_files.bat

--- 15_Copy_Fritzing_Requirement_DLL_files.bat
+++ 15_Copy_Fritzing_Requirement_DLL_files.bat
@@ -8,8 +8,8 @@ cd /d \00_fritzing
 
 copy .\libgit2\build64\Release\git2.dll .\release64
 copy .\zlib-src\build64\Release\zlib.dll .\release64
-copy .\quazip-6.5.3-1.4\bin\quazip1-qt6.dll .\release64
-copy C:\Qt\6.5.3\msvc2019_64\bin\Qt6Core5Compat.dll .\release64
+copy .\quazip-6.5.5-1.4\bin\quazip1-qt6.dll .\release64
+copy C:\Qt\6.5.5\msvc2019_64\bin\Qt6Core5Compat.dll .\release64
 
 rem for Simulate function
 copy .\Spice64_dll\dll-vs\*.dll .\release64
